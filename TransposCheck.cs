cda717db94d44d70c2f863d57b5c268707e6c155
KatFei <cundere@gmail.com>
2020-02-21 21:54:17 +0300

    Class for main method StringsChecker was added. ReadFromSource() and WriteToFile() methods were moved there.

--- TransposCheck.cs
+++ TransposCheck.cs
@@ -9,9 +9,11 @@ using System.Collections.Generic;
 
 namespace TransposeCheckNS
 {
-    public static class TransposCheck
+    public class TransposCheck
     {
-        /// <summary>Метод <c>IsTransposition</c> проверяет, является ли одна строка перестановкой другой.</summary>
+
+        /// <summary>Метод <c>IsTransposition</c> проверяет, является ли одна строка перестановкой другой.
+        /// Разницу в регистре не учитываем.</summary>
         /// <param name="s1"> Исходная строка</param>
         /// <param name="s2"> Cтрока, с которой сравниваем</param>
         /// <returns>Возвращает <c>true</c> или <c>false</c>, в зависимости от результата проверки.</returns>
@@ -19,6 +21,8 @@ namespace TransposeCheckNS
 
         public static bool IsTransposition(string s1, string s2)
         {
+            s1 = s1.ToLower();
+            s2 = s2.ToLower();
             if (s1.Length != s2.Length)
                 return false;
             Dictionary<char, int> itemCounts = new Dictionary<char, int>(EqualityComparer<char>.Default);
@@ -53,9 +57,81 @@ namespace TransposeCheckNS
             }
             return true;
         }
+        
 
         /// <seealso cref="IsTransposition(string,string)"></seealso>
 
+    }
+
+    public class StringsChecker
+    {
+
+        string srcStr1 = null;
+        string srcStr2 = null;
+        bool result = false; //Enum для result 
+
+        public string SrcStr1
+        {
+            get {    return srcStr1;     }
+        }
+        public string SrcStr2
+        {
+            get { return srcStr2; }
+        }
+
+        public void ReadFromSource(string path)
+        {
+            StreamReader sIn = null;
+            try
+            {
+                sIn = new StreamReader(path);
+                srcStr1 = sIn.ReadLine();
+                srcStr2 = sIn.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("\nRead from file failed: \n" + path);
+            }
+            finally
+            {
+                if (sIn != null) sIn.Close();
+            }
+        }
+
+        public bool IsTransposition()
+        {
+            result = TransposCheck.IsTransposition(SrcStr1, srcStr2);
+            return result;
+        }
+        public void WriteToFile(string path)
+        {
+            if ((SrcStr1 != null) && (srcStr2 != null))
+            {
+                result = TransposCheck.IsTransposition(SrcStr1, srcStr2);
+
+
+                StreamWriter sOut = null;
+                try
+                {
+                    sOut = new StreamWriter(path);
+                    if (sOut != null)
+                    {
+                        sOut.WriteLine(result);
+                        Console.WriteLine("\nResult successfully written to " + path);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nWrite to  file failed");
+                }
+                finally
+                {
+                    if (sOut != null) sOut.Close();
+                }
+
+            }
+
+        }
 
         /// <summary>
         /// Точка входа для приложения.
@@ -63,15 +139,15 @@ namespace TransposeCheckNS
         /// <param name="args"> Список аргументов командной строки</param>
         public static int Main(String[] args)
         {
-            string srcStr1 = null;
-            string srcStr2 = null;
-            bool result = false;
             string workDir = Environment.CurrentDirectory;
 
             string path = Directory.GetParent(workDir).Parent.FullName;
             Console.WriteLine(path);
             Console.ReadKey();
-            StreamReader sIn = null;
+            StringsChecker checkingStrings = new StringsChecker();
+            checkingStrings.ReadFromSource(path + "/Input.txt");
+            checkingStrings.WriteToFile(path + "/Output.txt");
+            /*StreamReader sIn = null;
             try
             {
                 sIn = new StreamReader(path + "/Input.txt");
@@ -112,11 +188,10 @@ namespace TransposeCheckNS
                 }
 
             }
-
+            */
 
             Console.ReadKey();
             return 0;
         }
     }
-
 }
